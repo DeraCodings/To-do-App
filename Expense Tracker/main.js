d93efc404729ec1c4cdfb0bccbd02c6788913b7f
DeraCodings <108424766+DeraCodings@users.noreply.github.com>
2022-08-04 00:18:53 +0100

    Added via upload
    Added new features

--- Expense Tracker/main.js
+++ Expense Tracker/main.js
@@ -4,8 +4,16 @@ const date = document.getElementById('date');
 const num = document.getElementById('number');
 const tableBody = document.getElementById('table-body');
 const error = document.getElementById('error');
+const tableFoot = document.querySelector('#table-foot');
+const total = document.querySelector('#total');
+const sum = document.querySelector('#sum');
+const deleteBtn = document.getElementById('delete-sum');
 
 const data = {};
+const amountArr = [];
+
+deleteBtn.style.display = 'none';
+sum.style.display = 'none';
 
 // spitting the data to the screen
 const spitData = () => {
@@ -27,15 +35,18 @@ const getData = () => {
     data['name'] = txt.value;
     data['date'] = date.value;
     data['amount'] = `$${num.value}`;
+    amountArr.push(parseInt(num.value));
     // console.log(data);
     spitData();
+
+    // sumAll();
 }
 
 // form validation
 const formValidate = () => {
     if ((txt.value === '' && num.value === '') || (txt.value !== '' && num.value === '') || (txt.value === '' && num.value !== '')) {
         // console.log('* are supposed to be empty');
-        error.innerHTML = '<strong>* cannot be empty</strong>';
+        error.innerHTML = '<strong>*cannot be empty*</strong>';
     }
     else {
         getData();
@@ -47,14 +58,23 @@ const formValidate = () => {
 form.addEventListener('submit', (e) => {
     e.preventDefault();
     formValidate();
+    setTimeout(() => {
+        deleteBtn.style.display = 'inline';
+        sum.style.display = 'inline';
+    }, 700)
 });
 
+// total.previousElementSibling.previousSibling
 // adding delete function to the delete button
-const deleteItem = (x) => {
+function deleteItem(x){
     x.parentElement.parentElement.remove();
-    // console.log('deleted');
+    let elemToRemove = amountArr.indexOf(parseInt(x.previousElementSibling));
+    amountArr.splice(elemToRemove, 1);
+    console.log(x.previousElementSibling);
 }
 
+// Array.prototype.indexOf();
+// txt.previousElementSibling
 // deleting the whole list
 
 const clearAll = () => {
@@ -67,8 +87,29 @@ const clearAll = () => {
     else {
         // parent.removeChild(children);
         tableBody.innerHTML = '';
+        total.innerText = '';
         // console.log('success');
     }
     // console.log('no list to delete');
     // error.innerHTML = '<small>cant delete empty list</small>';
 }
+
+// adding a sum function to the button
+const sumAll = () => {
+    /**How to check if a button is clicked */
+    /**
+     * if the button is clicked,
+     * I want to remove an element from the amountArr
+     * Specifically, the element of the table data that is the sibling of the button
+     */
+    let ans = amountArr.reduce((x, y) => parseInt(x) + parseInt(y), 0);
+    total.innerText = `$${ans}`;
+}
+
+//A function that deletes the total sum
+function deleteAll() {
+    total.innerText = 0;
+}
+
+
+console.log(amountArr);
