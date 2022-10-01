e65127ef8048951ebd0caa8b9be442b276dbf30e
Otuonye Chidera <108424766+DeraCodings@users.noreply.github.com>
2022-10-02 00:38:39 +0100

    Update script.js

--- AfroStore/script/script.js
+++ AfroStore/script/script.js
@@ -1,12 +1,11 @@
 let j = 0;
 
 const arr = [];
-
-arr[0] = '../images/quote1.png';
-arr[1] = '../images/quote2.png';
-arr[2] = '../images/quote3.png';
-arr[3] = '../images/quote4.png';
-arr[4] = '../images/quote5.png';
+arr[0] = './images/quote1.png';
+arr[1] = './images/quote2.png';
+arr[2] = './images/quote3.png';
+arr[3] = './images/quote4.png';
+arr[4] = './images/quote5.png';
 
 // const len = arr.length;
 
